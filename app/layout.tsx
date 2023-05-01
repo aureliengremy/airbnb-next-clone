2363ac1b30318b4af2021bed3150024158305b0c
Aurelien Gremy <gremy.aurelien@gmail.com>
2023-05-01 18:08:13 -0400

    feat(layout, UserMenu) add the RegisterModal component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+// import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
@@ -20,7 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel="btn ActionLabel" isOpen={true} title="Modal Title"/>
+          <RegisterModal/>
+          {/* <Modal actionLabel="btn ActionLabel" isOpen={true} title="Modal Title"/> */}
           <Navbar />
         </ClientOnly>
         {children}
